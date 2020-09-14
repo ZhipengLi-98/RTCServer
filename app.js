3cd90e287b8d00a8e8a468958c224dc5b684ef09
arsenalahm <clevercake7@gmail.com>
2020-09-14 14:13:30 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,128 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express')
+,	path = require('path')
+,	streams = require('./app/streams.js')();
+
+var favicon = require('serve-favicon')
+,	logger = require('morgan')
+,	methodOverride = require('method-override')
+,	bodyParser = require('body-parser')
+,	errorHandler = require('errorhandler');
+
+var app = express();
+
+var http = require('http');
+
+// all environments
+app.set('port', process.env.PORT || 2000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
+}
+
+// routing
+var routes = require('./app/routes.js')(app, streams);
+
+var server = app.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+/**
+ * Socket.io event handling
+ */
+require('./app/socketHandler.js')(io, streams);
+
+var pythonServer = http.createServer(app);
+
+var port = normalizePort(process.env.PORT || '1000');
+pythonServer.listen(port);
+pythonServer.on('error', onError);
+pythonServer.on('listening', onListening);
+pythonServer.on('connection', onConnection);
+
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  console.log(addr);
+  var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+}
+
+global.flag = false;
+
+function onConnection(client) {
+  client.on('data', function (data) {
+    console.log(data.toString('ascii'));
+    var msg = data.toString('ascii');
+    if (msg == 'Start') {
+      console.log("index");
+      global.flag = true;
+    }
+  });
+  client.write("Received");
+}
